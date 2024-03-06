2c07b7a526b3e4f4440321854ccd298b1e0efa53
Jennifer Smale <JenniferSmale7@gmail.com>
2024-03-06 14:30:18 -0500

    created JdbcAccountDao

---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,36 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.exception.DaoException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+
+public class JdbcAccountDao implements AccountDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public BigDecimal getBalance(int userID) {
+        BigDecimal balance = BigDecimal.valueOf(0);
+        String sql = "SELECT balance FROM account AS a " +
+                "JOIN tenmo_user AS tu ON tu.user_id = a.user_id " +
+                "WHERE user_id = ?";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userID);
+            if (results.next()) {
+                balance = (BigDecimal) results;
+            }
+        }
+        catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return balance;
+    }
+}
