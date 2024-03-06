6b6d8f4250514a23e003c87bde6094981e86c339
Jennifer Smale <JenniferSmale7@gmail.com>
2024-03-06 15:09:47 -0500

    updated create method

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -1,8 +1,12 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.TransferDao;
+import com.techelevator.tenmo.exception.DaoException;
 import com.techelevator.tenmo.model.Transfer;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +25,21 @@ public class TransferController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping()
-    public Transfer create(){
+    @PostMapping("transfer")
+    public Transfer create(@Valid @RequestBody Transfer transfer){
         Transfer newTransfer = null;
+        try {
+            newTransfer = transferDao.create(transfer);
+        }
+        catch (CannotGetJdbcConnectionException ex) {
+            throw new DaoException("Connection error.", ex);
+        }
+        catch (DataIntegrityViolationException ex) {
+            throw new DaoException("Error with data integrity.", ex);
+        }
+        catch (BadSqlGrammarException ex) {
+            throw new DaoException("Please review your SQL string.", ex);
+        }
         return newTransfer;
     }
 
