28d77bc20c3df1e2d89e093d370605fa84b4d913
Jennifer Smale <JenniferSmale7@gmail.com>
2024-03-07 10:46:19 -0500

    add Account model and AccountService to client

---
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -0,0 +1,45 @@
+package com.techelevator.tenmo.services;
+
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+public class AccountService {
+    private static final String API_BASE_URL = "http://localhost:8080/";
+    private final RestTemplate restTemplate = new RestTemplate();
+    private String authToken = null;
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public BigDecimal getBalance(int userId){
+        BigDecimal balance = BigDecimal.valueOf(0);
+        try {
+            ResponseEntity<Account> response = restTemplate.exchange(API_BASE_URL + "balance" + userId, HttpMethod.GET, makeAuthEntity(), Account.class);
+            Account account = response.getBody();
+            if (account != null) {
+                balance = account.getBalance();
+            }
+        }
+        catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return balance;
+    }
+
+    private HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(headers);
+    }
+
+}
