1decf9042e21fdeaa617c661fcc68ca49a5baa3d
Ev <e-meredith[A[C[C[C[C Meredith[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D@onu.edu>
2024-03-07 09:54:43 -0500

    implement get all pending transfers

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,34 +1,77 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.exception.DaoException;
 import com.techelevator.tenmo.model.Transfer;
+import org.springframework.cache.annotation.CacheAnnotationParser;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class JdbcTransferDao implements TransferDao{
     private final String TRANSFER_SELECT = "SELECT t.transfer_id, t.transfer_status_id, t.transfer_status_id, t.account_from, t.account_to, t.amount FROM transfer AS t";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @Override
     public List<Transfer> getAll(int userId) {
         //TODO: getAll
         List<Transfer> transfers = new ArrayList<>();
         String sql = TRANSFER_SELECT +
                 "WHERE account_from IN (SELECT account_id FROM account WHERE user_id = ?) OR account_to IN (SELECT account_id FROM account WHERE user_id = ?)";
-        return null;
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+            while (results.next()){
+                Transfer transferResult = mapRowsToTransfer(results);
+                transfers.add(transferResult);
+            }
+        } catch (CannotGetJdbcConnectionException e){
+            throw new DaoException("Cannot connect to database.", e);
+        }
+        return transfers;
     }
 
     @Override
     public Transfer getById(int id, int userId) {
         //TODO: getById
-        return null;
+        Transfer transfer = null;
+        String sql = TRANSFER_SELECT +
+                "WHERE transfer_id = ?;";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+            if (results.next()){
+                transfer = mapRowsToTransfer(results);
+            }
+        } catch (CannotGetJdbcConnectionException e){
+            throw new DaoException("Cannot connect to database.", e);
+        }
+        return transfer;
     }
 
     @Override
     public Transfer create(Transfer transfer) {
         //TODO: create
-        return null;
+        Transfer newTransfer = new Transfer();
+        String sql = "INSERT INTO transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount) VALUES (?, ?, ?, ?, ?) RETURNING transfer_id;";
+        try {
+            int newTransferId = jdbcTemplate.queryForObject(sql, int.class, transfer.getTypeId(), transfer.getStatusId(), transfer.getFromAccount(), transfer.getToAccount(), transfer.getAmount());
+            newTransfer = getById(newTransferId);
+        } catch (CannotGetJdbcConnectionException e) {
+        throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+        throw new DaoException("Data integrity violation", e);
+    }
+        return newTransfer;
     }
 
     @Override
