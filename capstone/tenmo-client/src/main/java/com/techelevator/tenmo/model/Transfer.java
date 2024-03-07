6608eb940b4bcdcb70c9db90fbff38a02a668824
Ev <e-meredith[A[C[C[C[C Meredith[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D@onu.edu>
2024-03-07 10:26:03 -0500

    transfer service and model created

---
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -0,0 +1,14 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Transfer {
+    private int id;
+    private int typeId;
+    private int statusId;
+    private int fromAccount;
+    private int toAccount;
+    private BigDecimal amount;
+
+
+}
