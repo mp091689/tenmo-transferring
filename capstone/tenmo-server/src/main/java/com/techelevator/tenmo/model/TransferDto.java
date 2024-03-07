49bccde0c108051f7547e0c91c4a31f90516e307
Mykyta <mr.mykyta.popov@gmail.com>
2024-03-07 16:33:49 -0500

    fix transfer service

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferDto.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferDto.java
@@ -3,20 +3,16 @@ package com.techelevator.tenmo.model;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 
 public class TransferDto {
 
-    @NotEmpty
     @Min(value = 0L)
     private int userId;
 
-    @NotEmpty
     @Min(value = 0L)
     private int typeId;
 
-    @NotEmpty
     @DecimalMin(value = "0.0", inclusive = false)
     @Digits(integer=10, fraction=2)
     private BigDecimal amount;
